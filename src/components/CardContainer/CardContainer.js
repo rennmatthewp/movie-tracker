24592e525447cb4bfa09e247ac0d5b1b94439bef
Matthew Renn <rennmatthewp@gmail.com>
2017-12-13 14:03:51 -0700

    pretify CardContainer.js

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-const CardContainer = (props) => {
-  const mappedCards = props.movies.map( (movie, index) => { 
-      return (
-        <div key={`movie-${index}`}>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt='movie poster'/>
-          <h1>{movie.title}</h1>
-        </div>
-      );
+const CardContainer = props => {
+  const mappedCards = props.movies.map((movie, index) => {
+    return (
+      <div key={`movie-${index}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt="movie poster"
+        />
+        <h1>{movie.title}</h1>
+      </div>
+    );
   });
 
-  return (
-    <div className='CardContainer'>
-      {mappedCards}
-    </div>
-  )
-}
+  return <div className="CardContainer">{mappedCards}</div>;
+};
 
-const mapStateToProps = state => ({ movies: state.movies })
+const mapStateToProps = state => ({ movies: state.movies });
 // const mapDispatchToProps = (dispatch) => ({
 //   handleFetch: (moviesArray) => dispatch(fetchMovies(moviesArray))
 // })
