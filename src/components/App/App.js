4afd8cde2b2f9e0ee767cd935f3b78b19220ff56
Matthew Renn <rennmatthewp@gmail.com>
2017-12-12 17:44:43 -0700

    imports apiKey, fetches now_playing array, renders movie titles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
+import { apiKey } from '../../helper/.apiKey';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieArray: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const initialFetch = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
+      );
+      const parsedData = await initialFetch.json();
+      const movieArray = parsedData.results;
+      this.setState({ movieArray });
+    } catch (error) {
+      console.log('error:', error);
+    }
+  }
+
+  mappedMovies = () => {
+    return this.state.movieArray.map( (movie, i) => {
+      console.log('movie:', movie);
+      
+      return (
+        <div key={`movie-${i}`}>
+          <h1>{movie.title}</h1>
+        </div>
+      )
+  });
+  };
+
   render() {
-    return (
-      <div className="App">
-        hello julies and the matt
-      </div>
-    );
+    return <div className="App">{this.mappedMovies()}</div>;
   }
 }
 
