276b5be3d56dd93f1a9d3e62453fb3014a4837be
Julie Dursema <jmdursema@gmail.com>
2017-12-15 10:37:53 -0700

    display current user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+
+
 const Header = props => {
+  let username
+  if (props.username.data){
+    username = <h3>Hello, {props.username.data.name}!</h3>
+  }
+
   return (
     <div>
       <Link to='/' className='home-link'>
         <h1>MovieTracker</h1>
       </Link>
+      {username}
       <Link to='/createaccount' className='createaccount'>
         Create Account
       </Link>
@@ -18,7 +26,9 @@ const Header = props => {
   );
 };
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  username: state.user
+});
 
 
 
--- src/helper/apiHelper.test.js
+++ src/helper/apiHelper.test.js
@@ -22,3 +22,25 @@ describe('apiHelper', () => {
 
   xit('should return and error if the fetch fails', () => {});
 });
+
+// describe('postNewAccount', () => {
+//   beforeEach(() => {
+//     window.fetch = jest.fn().mockImplementation(() =>
+//       Promise.resolve({
+//         json: () =>
+//           Promise.resolve({
+//            username: 'Julie'
+//           })
+//       })
+//     );
+//   });
+//   it('should return a the userObj', async () => {
+//     const fetch = await apiFetch();
+//     const mockUserObj= { username: 'Julie' };
+
+//     // expect(typeof fetch).toEqual('object');
+//     expect(fetch).toEqual(mockUserObj);
+//   });
+// })
+
+
