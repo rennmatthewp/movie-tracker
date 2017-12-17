8183d9bd821483608c42cc71aa545493304eaeff
Matthew Renn <rennmatthewp@gmail.com>
2017-12-17 09:43:28 -0700

    adds content to movie card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,29 +8,31 @@ const Card = props => {
     movie_id: props.id,
     release_date: props.releaseDate,
     vote_average: props.voteAvg,
-    overview:props.overview
-  }
+    overview: props.overview
+  };
 
   return (
-    <div 
-      className="Card" 
-      onClick={()=> {
-        if(!props.user.id){
-          props.history.push('/login');
-        } else {
-          props.handleFav(props.user.id, movieObj);
-        }
-      }}>
+    <div className="Card">
+      <button
+        onClick={() => {
+          if (!props.user.id) {
+            props.history.push('/login');
+          } else {
+            props.handleFav(props.user.id, movieObj);
+          }
+        }}>Fave!
+      </button>
       
       <img
         src={`https://image.tmdb.org/t/p/w500${props.posterPath}`}
         alt={`Poster for ${props.title}`}
       />
-      <h1>{props.title}</h1>
+      <h3>{props.title}</h3>
+      <h4>Released: {props.releaseDate}</h4>
+      <h4>Rating: {props.voteAvg}</h4>
+      <h4>{props.overview}</h4>
     </div>
   );
 };
-
-
 
 export default Card;
